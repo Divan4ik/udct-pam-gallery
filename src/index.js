e554215d51a98c9f36827af46473719e1860f113
admin@debian.msk <divangt@mail.ru>
2017-11-20 02:15:17 +0300

    disable webhook. to complicated

--- src/index.js
+++ src/index.js
@@ -2,16 +2,5 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Welcome!'));
-app.get('/update', (req, res) => {
 
-    exec('./../update.sh',
-    function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-        if (error !== null) {
-         console.log('exec error: ' + error);
-        }
-    })
-    res.send('updating');
-});
 app.listen(8000, () => console.log('listening on port 8000'));
